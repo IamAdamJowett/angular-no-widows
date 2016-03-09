a22e9ff59db942be90a50958b755ccef5a608c2b
Adam Jowett <adam@mettro.com.au>
2016-03-09 13:30:32 +1000

    Updated to allow both attributes and element text processing

--- no-widows.directive.js
+++ no-widows.directive.js
@@ -1,3 +1,5 @@
+/* global angular */
+
 (function() {
     'use strict';
 
@@ -10,20 +12,26 @@
             restrict: 'A',
             replace: true,
             link: function($scope, elem, attr) {
-                var watcher = $scope.$watch(attr.noWidow, function(newVal, oldVal) {
+
+                var watcher = $scope.$watch(attr.noWidows, function(newVal, oldVal) {
                     if (newVal) {
-                        var txt = newVal,
-                            tag = elem[0].nodeName.toLowerCase(),
-                            wordArray = txt.split(" ");
-
-                        if (wordArray.length > 1) {
-                            wordArray[wordArray.length - 2] += "&nbsp;" + wordArray[wordArray.length - 1];
-                            wordArray.pop();
-                        }
-
-                        elem.html(wordArray.join(" "));
+                        processText(newVal);
+                    } else {
+                        processText(elem.html());
                     }
                 });
+
+                function processText(txt) {
+                    var tag = elem[0].nodeName.toLowerCase(),
+                        wordArray = txt.split(" ");
+
+                    if (wordArray.length > 1) {
+                        wordArray[wordArray.length - 2] += "&nbsp;" + wordArray[wordArray.length - 1];
+                        wordArray.pop();
+                    }
+
+                    elem.html(wordArray.join(" "));
+                }
             }
         };
     }
